425ad22c216b16bf0bedb53e16ae1c74457df820
s3604175 <50316489+s3604175@users.noreply.github.com>
2019-08-26 22:19:03 +1000

    Register and Login with database
    Read and follow the readme.txt to test out the register and login functions, as well as opening a server that is connected to the mongoDB cloud database.

---
+++ readme.txt
@@ -0,0 +1,7 @@
+Register and Login to database
+
+1. Download these folders containing user.js, api.js and server.js and files into a folder.
+2. Use node.js to download express, body-parser and mongoose modules in terminal. Use cmd and change the directory to the folder from step 1, then use 'npm install express body-parser mongoose --save'.
+3. Use the command 'npm server' to turn on the server. It should connect to localhost and mongodb.
+4. Go on web browser and enter 'localhost:3000'.
+5. To test the register and login functionality, refer to this playlist 'https://www.youtube.com/playlist?list=PLC3y8-rFHvwg2RBz6UplKTGIXREj9dV0G' for better understanding. It is based on this tutorial.
---
+++ routes/api.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const mongoose = require('mongoose')
+
+const db = "mongodb+srv://ppcryptouser:1234@cryptogame-bhtee.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(db, err => {
+    if (err) {
+        console.error('Error!' + err)
+    }
+    else {
+        console.log('Connect to mongodb')
+    }
+})
+
+router.get('/', (req, res) => {
+    res.send('From API route')
+})
+
+router.post('/register', (req, res) => {
+   let userData = req.body
+   let user = new User(userData)
+   user.save((error, registeredUser) => {
+       if (error) {
+           console.log(error)
+       }
+       else {
+           res.status(200).send(registeredUser)
+       }
+   })
+})
+
+router.post('/login', (req, res) => {
+    let userData = req.body
+
+    User.findOne({email: userData.email}, (error, user) => {
+        if(error) {
+            console.log(error)
+        }
+        else {
+            if (!user) {
+                res.status(401).send('Invalid email')
+            }
+            else if(user.password !== userData.password){
+                res.status(401).send('Invaild password')
+            }
+            else {
+                res.status(200).send(user)
+            }
+        }
+    })
+})
+
+module.exports = router
