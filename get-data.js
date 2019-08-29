16ddf74dd6bc5dfced38c5af5a424dddefe638a6
Justin <s3454507@student.rmit.edu.au>
2019-08-29 23:54:53 +1000

    getData Improvements
    Added cleaner JSON formating

--- get-data.js
+++ get-data.js
@@ -1,28 +1,36 @@
 const https = require('https')
 const fs = require('fs')
 
-const getData = ()=>{
+const getData = () => {
+  var options = {
+      "method": "GET",
+      "hostname": "rest.coinapi.io",
+      "path": "/v1/exchangerate/BTC",
+      "headers": {'X-CoinAPI-Key': '67FA93DD-C576-4CAE-9DC6-5B15559C436B'}
+    }
 
-var options = {
-    "method": "GET",
-    "hostname": "rest.coinapi.io",
-    "path": "/v1/exchangerate/BTC",
-    "headers": {'X-CoinAPI-Key': '67FA93DD-C576-4CAE-9DC6-5B15559C436B'}
-  }
-  
-  var request = https.request(options, function (response) {
-    var chunks = []
-    response.on("data", function (chunk) {
-      chunks.push(chunk)
-      const stuff = JSON.stringify(chunk)
-      fs.writeFileSync('crypto-data.json', chunk)
-      console.log('Current BTC rates successfully retrieved')
-    });
-  });
-  
+  const request = https.request(options, (response) => {
+      let data = ''
+
+      response.on('data', (chunk) => {
+          data = data + chunk.toString()
+      })
+
+      response.on('end', () => {
+          const body = JSON.parse(data)
+          const cryptoData = JSON.stringify(body, null, 2)
+          fs.writeFileSync('crypto-data.json', cryptoData)
+          console.log('Current BTC rates successfully retrieved')
+      })
+
+  })
+  request.on('error', (error) => {
+      console.log(error)
+  })
+
   request.end()
 }
 
 module.exports = {
-    getData: getData
+  getData: getData
 }
