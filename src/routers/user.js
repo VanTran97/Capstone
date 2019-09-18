28f809467345a95a21933a7122ec2e0e60ac733f
Justin <s3454507@student.rmit.edu.au>
2019-09-18 12:58:14 +1000

    Login user
    Login user api endpoint implementation

--- src/routers/user.js
+++ src/routers/user.js
@@ -16,4 +16,15 @@ router.post('/users', async (req, res) => {
     }
 })
 
+// Login user
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
 module.exports = router
