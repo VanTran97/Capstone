067245d17d0f671432eb5aae045403dcd3b218fd
Raymond <s3609727@users.noreply.github.com>
2019-11-10 21:47:30 +1100

    Update About.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -13,7 +13,12 @@ class About extends Component {
         <br></br><br></br>
         <p3>1. A user will have to create and account to gain access to the crypto market. Once a user creates an account they will then gain access to the websites features such as the crypto market, graphs and user profile page. The users account will also start with $1,000,000 bogus money that they can use to invest into the crypto market.</p3>
         <br></br><br></br>
-        <p3>2. Once a user buys crypto</p3>
+        <p3>2. Once a user buys and sells crypto currencey their total balance will reduce or increase depending on whether you buy or sell</p3>
+        <br></br>
+        <p3>3. A user can not go into the negatives, if they have no bogus money they will have to sell their crypto shares in order to gain money</p3>
+        <br></br>
+        <p3>4. The game will be incremented in seasons 6 months being a season and at the end of every season the entire game resets to $1,000,000 bogus money</p3>
+        <br></br>
       </div>
     );
   }
